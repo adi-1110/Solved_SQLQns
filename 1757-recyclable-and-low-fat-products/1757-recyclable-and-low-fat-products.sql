19e837207777afda464097120529f9f0177c7fe9
adi-1110 <adityapugal05@gmail.com>
2026-07-19 22:07:54 +0530

    Time: 216 ms (99.03%), Space: 0B (100%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,9 @@
+SELECT product_id
+FROM Products
+Where low_fats = 'Y' AND recyclable = 'Y'
+
+/*
+Synced seamlessly with LeetHub Pro
+Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+*/
