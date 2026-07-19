fe0f817075b93e2adc43a4390aa708ca5e4bd224
adi-1110 <adityapugal05@gmail.com>
2026-07-19 22:17:18 +0530

    Time: 581 ms (12.47%), Space: 0B (100%) - LeetHub

---
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,13 @@
+-- Write your PostgreSQL query statement below
+select
+    u.unique_id,
+    e.name
+from Employees as e
+left join EmployeeUNI as u
+on e.id=u.id
+
+/*
+Synced seamlessly with LeetHub Pro
+Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+*/
